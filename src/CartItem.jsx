9f101c004f11be57b7fba28fcf6543b226d29fe0
LaurenHerregodts <lauren.herregodts@cellcarta.com>
2025-05-09 14:31:18 -0400

    Update CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -4,41 +4,55 @@ import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
-  const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
+    const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
-    let total = 0;
+    const calculateTotalAmount = () => {
+        let total = 0;
 
-    cart.forEach((item) => {
-        const quantity = item.quantity;
-        const cost = parseFloat(item.cost.substring(1));
-        total += quantity * cost;
-    });
+        cart.forEach((item) => {
+            const quantity = item.quantity;
+            const cost = parseFloat(item.cost.substring(1));
+            total += quantity * cost;
+        });
 
-    return total;
-  };
+        return total;
+    };
 
-  const handleContinueShopping = (e) => {
-   
-  };
+    const handleContinueShopping = (e) => {
+        onContinueShopping(e)
+    };
 
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
+    const handleIncrement = (item) => {
+        dispatch(
+            updateQuantity(
+                {name: item.name, quantity: item.quantity + 1}
+            )
+        )
+    };
 
-  const handleIncrement = (item) => {
-  };
+    const handleDecrement = (item) => {
+        dispatch(
+            updateQuantity(
+                {name: item.name, quantity: item.quantity - 1}
+            )
+        )
+    };
 
-  const handleDecrement = (item) => {
-   
-  };
-
-  const handleRemove = (item) => {
-  };
+    const handleRemove = (item) => {
+        dispatch(removeItem(item.name))
+    };
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
-  };
+    const calculateTotalCost = (item) => {
+        const unitPrice = parseFloat(item.cost.substring(1));
+        return (unitPrice * item.quantity)
+    };
 
   return (
     <div className="cart-container">
