bf99da741a7aec7eb8d59de5a0650d46ce875acc
LaurenHerregodts <lauren.herregodts@cellcarta.com>
2025-05-09 10:43:44 -0400

    Define reducer functions in CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,16 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
+        const { name, image, cost } = action.payload; 
+        if (existingItem) {
+            // If item alre        } else {
+            // If item does not exist, add it to the cart with quantity 1
+            stat    },
     removeItem: (state, action) => {
     },
     updateQuantity: (state, action) => {
 
-    
+
+state.items = state.items.filter(item => item.name !== action.payload);            
     },
-  },
-});
+
+structure the product name and new quantity from the action payload
+        // Find the item in the cart that matches the given name
+        const itemToUpdate = state.items.find(item => item.name === name);
+             (itemToUpdate) {
+         });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
